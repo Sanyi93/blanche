f994b8b9f530e96c290f045dffad619f76b5a506
Sanyi93 <sandor.tomas93gmail.com>
2024-12-28 17:13:24 -0500

    followersCount files added

---
+++ followersCount/followers_count.js
@@ -0,0 +1,19 @@
+let count = 0; //initializing count to 0
+
+function increaseCount(){
+    count++; //incrementing count by 1
+    displayCount();
+    checkCountValue();
+}
+
+function displayCount(){
+    document.getElementById('countDisplay').innerHTML=count; //displaying the count in the html  
+}
+
+function checkCountValue(){
+    if(count === 10){
+        alert("Your instagram post gained 10 followers! Congratulations!");
+    } else if (count === 20){
+        alert("Your Instagram post gained 20 followers! Keep it up!");
+    }
+}
