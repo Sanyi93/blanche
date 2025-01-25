cae729ee70b550e5e70c45459dd824d6d199d486
sanyi93 <sandor.tomas93@gmail.com>
2025-01-24 19:36:08 -0500

    todoList added

---
+++ TodoList/todo_list.js
@@ -0,0 +1,44 @@
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
+const clearCompletedBtn = document.getElementById("clearCompletedBtn");
+
+//array where tasks are to be pushed
+let tasks = [];
+
+//adding/pushing tasks
+function addTask() {
+    const taskText = taskInput.value.trim();
+    if (taskText !== "") {
+        tasks.push({ text: taskText});
+        taskInput.value = "";
+        displayTasks();
+    }
+}
+
+function displayTasks() {
+    taskList.innerHTML = "";
+    tasks.forEach((task, index) => {
+        const li = document.createElement("li");
+        li.innerHTML = `<input type="checkbox" id="task-${index}" ${task.completed ? "checked" : ""}>
+            <label for="task-${index}">${task.text}</label>`;
+        li.querySelector("input").addEventListener("change", () => toggleTask(index));
+        taskList.appendChild(li);
+    });
+}
+
+function toggleTask(index) {
+    tasks[index].completed = !tasks[index].completed;
+    displayTasks();
+}
+
+//filtering on TASKS list; filtering only the one that are not completed
+function clearCompletedTasks() {
+    tasks = tasks.filter(task => !task.completed);
+    displayTasks();
+}
+
+addTaskBtn.addEventListener("click", addTask);
+clearCompletedBtn.addEventListener("click", clearCompletedTasks);
+
+displayTasks();
